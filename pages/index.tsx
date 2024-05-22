d3cf9f64c75d7770701172a14c3cf418b2f8d5ff
Vitor Gonçalves <vitorgoncalves57@gmail.com>
2024-05-22 11:41:38 +0100

    Added the send form js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,77 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import emailjs from "@emailjs/browser";
+import React, { useRef, useState } from "react";
 
-const IndexPage = () => (
-  <Layout title="Coming Soon - Clean Coming Soon Page (Tailwind CSS, NextJs & HTML5) created by themeptation.net">
-    <img
-      src="/images/xera.svg"
-      alt="Themeptation "
-      className="absolute h-96 -top-20 -right-16 lg:right-5 lg:top-10 animate-blob"
-    />
-    <img
-      src="/images/shapes.svg"
-      alt="hero"
-      className="absolute w-full left-24 bottom-24 animate-blob2"
-    />
-    <div className="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-900">
-      <div className="text-center space-y-10">
-        <h3 className="font-light text-xl uppercase tracking-wider">
-          Coming soon
-        </h3>
-        <h1 className="text-7xl lg:text-9xl font-extrabold">We’r blowing up</h1>
-        <p className="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl lg:mx-auto">
-          We`re under construction. Check back for an update soon. Stay in
-          touch!
-        </p>
+const IndexPage = () => {
+  const form = useRef();
+  const [emailInput, setEmailInput] = useState("");
+  const sendEmail = (e) => {
+    e.preventDefault();
+    var formData = new FormData();
+    formData.append("message", "message");
+    emailjs
+      .sendForm("service_wm7uypr", "template_vxe3qt1", form.current, {
+        publicKey: "7sIubnEZ0-5J8Xopo",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+  };
+  return (
+    <Layout title="Coming Soon - Clean Coming Soon Page (Tailwind CSS, NextJs & HTML5) created by themeptation.net">
+      <img
+        src="/images/xera.svg"
+        alt="Themeptation "
+        className="absolute h-96 -top-20 -right-16 lg:right-5 lg:top-10 animate-blob"
+      />
+      <img
+        src="/images/shapes.svg"
+        alt="hero"
+        className="absolute w-full left-24 bottom-24 animate-blob2"
+      />
+      <div className="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-900">
+        <div className="text-center space-y-10">
+          <h3 className="font-light text-xl uppercase tracking-wider">
+            Coming soon
+          </h3>
+          <h1 className="text-7xl lg:text-9xl font-extrabold">
+            We’r blowing up
+          </h1>
+          <p className="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl lg:mx-auto">
+            We`re under construction. Check back for an update soon. Stay in
+            touch!
+          </p>
+        </div>
+        <form
+          ref={form}
+          className="relative z-10 mx-10 lg:max-w-xl lg:mx-auto"
+          onSubmit={sendEmail}
+        >
+          <input
+            name="message"
+            value={emailInput}
+            onChange={(e) => {
+              console.log("v ", e);
+              setEmailInput(e.target.value);
+            }}
+            type="email"
+            className="w-full text-2xl font-light text-gray-900 placeholder-gray-500 py-5 pl-5 pr-36 lg:pr-44 rounded-xl"
+          />
+          <button
+            type="submit"
+            className="absolute top-1 right-1 bottom-1 px-4 lg:px-10 text-xl font-semibold bg-gray-900 text-white rounded-xl transition ease-in-out duration-500 hover:bg-red-500"
+          >
+            Notify me
+          </button>
+        </form>
       </div>
-      <form className="relative z-10 mx-10 lg:max-w-xl lg:mx-auto">
-        <input
-          type="text"
-          placeholder="hi@themeptation.net"
-          className="w-full text-2xl font-light text-gray-900 placeholder-gray-500 py-5 pl-5 pr-36 lg:pr-44 rounded-xl"
-        />
-        <button className="absolute top-1 right-1 bottom-1 px-4 lg:px-10 text-xl font-semibold bg-gray-900 text-white rounded-xl transition ease-in-out duration-500 hover:bg-red-500">
-          Notify me
-        </button>
-      </form>
-    </div>
+      {/*}
     <div className="absolute bottom-5">
       <ul className="flex space-x-3">
         <li>
@@ -107,7 +144,9 @@ const IndexPage = () => (
         </li>
       </ul>
     </div>
-  </Layout>
-);
+*/}
+    </Layout>
+  );
+};
 
 export default IndexPage;
